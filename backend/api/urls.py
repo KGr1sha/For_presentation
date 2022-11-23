b1929855e7ac233749cb750ec8ecf26179300176
KGr1sha <krylov.grigory@gmail.com>
2022-11-23 21:07:27 +0300

    second commit

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
-from .views import BookViewSet, StudentViewSet, JournalViewSet
+from .views import BookViewSet, JournalViewSet, get_queryset
 from rest_framework import routers
 
 router = routers.SimpleRouter()
 router.register(r'book', BookViewSet)
-router.register(r'student', StudentViewSet)
 router.register(r'journal', JournalViewSet)
 
 
 urlpatterns = [
     path('api/v1/', include(router.urls)),
+    path('class/<int:id>/', get_queryset)
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,7 @@
+from django.views.generic import ListView
 from rest_framework import viewsets
+from rest_framework.response import Response
+
 from .models import Book, Student, Journal
 from .serializers import BookSerializer, StudentSerializer, JournalSerializer
 
@@ -8,9 +11,8 @@ class BookViewSet(viewsets.ModelViewSet):
     serializer_class = BookSerializer
 
 
-class StudentViewSet(viewsets.ModelViewSet):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
+def get_queryset(self):
+    return Response(Student.objects.filter(class_number=self.kwargs['id']))
 
 
 class JournalViewSet(viewsets.ModelViewSet):
